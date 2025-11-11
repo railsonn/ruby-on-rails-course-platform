d76c69a38fce4bf9dffe373f2b96fbe1e7ec1360
railsonn <railsongamer99@gmail.com>
2025-11-10 21:15:45 -0300

    add bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/css/bootstrap"
+require("stylesheets/application.css")
