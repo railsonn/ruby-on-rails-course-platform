46535ae2c0e750a571e38046775f4d47e2d3b4bc
railsonn <railsongamer99@gmail.com>
2025-11-13 20:36:24 -0300

    add haml rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "static_pages#landing_page"
+  root "home#index"
+  get "static_pages/landing_page"
   get "static_pages/pivacy_policy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
