d15de4b637ee2805978e005646ae4c15c3f55298
railsonn <railsongamer99@gmail.com>
2025-12-06 15:06:58 -0300

    estyling courses/edit and courses//new

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index]
   def index
     @course = Course.all.limit(3)
     @latest_couses = Course.all.limit(3).order(created_at: :desc)
