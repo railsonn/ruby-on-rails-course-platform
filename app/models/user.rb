e6cf3ef1a3a8da7ea3765cd347212bc7ab5dbc2a
railsonn <railsongamer99@gmail.com>
2025-12-09 21:14:36 -0300

    adding confirmable to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
 
+
   def to_s
     email
   end
--- db/migrate/20251209235046_add_confirmable_to_devise.rb
+++ db/migrate/20251209235046_add_confirmable_to_devise.rb
@@ -5,6 +5,5 @@ class AddConfirmableToDevise < ActiveRecord::Migration[8.0]
     add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :unconfirmed_email, :string
     add_index :users, :confirmation_token, unique: true
-    User.update_all confirmed_at: DateTime.now
   end
 end
