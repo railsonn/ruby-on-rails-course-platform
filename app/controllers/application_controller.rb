90dd06a78ea3c0b59c2c6c9a6237b25f1715c0bb
railsonn <railsongamer99@gmail.com>
2025-12-16 14:42:52 -0300

    Adding ransack to an already configured navbar.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_global_variables, if: :user_signed_in?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+
+  def set_global_variables
+    @q = Course.ransack(params[:q])
+    @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search) #navbar search
+  end
 end
