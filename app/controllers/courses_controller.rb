8bb23db2b12c73b2635a9da62f66dad6103b6014
railsonn <railsongamer99@gmail.com>
2025-12-22 21:14:02 -0300

    adding validates in role

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -13,7 +13,7 @@ class CoursesController < ApplicationController
     #   @q = Course.ransack(params[:q])
     #   @courses = @q.result.includes(:user)
     # end
-
+ 
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
     @courses = @ransack_courses.result.includes(:user) 
   end
