b1301d1b28728686f47b7d416b47edc8cb24e7fb
railsonn <railsongamer99@gmail.com>
2025-12-22 22:16:29 -0300

    add gem pundit and athorized in edit

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem 'ransack'
 gem 'public_activity'
 # gem rolify
 gem "rolify"
+# gem pundit
+gem "pundit"
+
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+
   include PublicActivity::StoreController #save current_user using gem public_activity
   
   before_action :set_global_variables, if: :user_signed_in?
@@ -7,9 +12,15 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
 
-
   def set_global_variables
     @q = Course.ransack(params[:q])
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search) #navbar search
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back_or_to(root_path)
   end
 end
